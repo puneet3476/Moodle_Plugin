2f827b130be0a0fcc4d5f216c66f65a3e1660564
shuvo151dey <48350056+shuvo151dey@users.noreply.github.com>
2020-08-05 22:16:24 +0530

    Update createfolder.php

--- basic/createfolder.php
+++ basic/createfolder.php
@@ -9,10 +9,10 @@ if (empty($folder_name) or empty($database_name) ) {
   die();
 }
 
-$dir_name="/var/www/html/Moodle_Plugin/".$course_name."/".$folder_name;
+$dir_name="/opt/lampp/htdocs/Moodle_Plugin/".$course_name."/".$folder_name;
 
 // Store the path of source file
-$source =   "/var/www/html/Moodle_Plugin/base";
+$source =   "/opt/lampp/htdocs/Moodle_Plugin/base";
 $hyperlink=$urla.$appache_localhost_port."/".$course_name."/".$folder_name."/".$urlb;
 
 // Create database
