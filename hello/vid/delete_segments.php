b1636f3ea0a3bb849a6a29ec61e1fa6aa8217502
shuvo151dey <shuvo151dey@gmail.com>
2020-08-06 22:16:19 +0530

    Removed folder from repository

--- hello/vid/delete_segments.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require ("connect.php");
-  $name=$_GET['name'];
-  $link->query("DELETE FROM segments WHERE segments_name='".$name."'");
-  header ($urla.$appache_localhost_port.$course.$folder.'segments.php');
-
-?>
-
