9105aa0b97c3cb1927d033bee185f7a598425d9d
Puneet Khandelwal <50475554+puneet3476@users.noreply.github.com>
2020-08-19 16:54:09 +0530

    rename file edited

--- basic/rename.php
+++ basic/rename.php
@@ -9,13 +9,13 @@ $course_name=$_POST['course_name'];
     ?>
     <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.css">
     <script src="https://kit.fontawesome.com/361990fe0a.js" crossorigin="anonymous"></script>
-
+<br><br><br><br><br><br><br><br>
 <body class="gradient">
        <h3>Enter your new video name</h3>
       <form method="POST" action="rename_video.php" role="form">
-          <input type="text" name="video_name" value="<?php echo($video_name) ?>">
-          <input type="text" name="database_name" value="<?php echo($database_name) ?>">
-          <input type="text" name="course_name" value="<?php echo($course_name) ?>">
+          <input type="text" name="video_name" value="<?php echo($video_name) ?>" style="display: none;">
+          <input type="text" name="database_name" value="<?php echo($database_name) ?>" style="display: none;">
+          <input type="text" name="course_name" value="<?php echo($course_name) ?>" style="display: none;">
           <input type="text" align="center"  name="new_folder_name" placeholder="New Lecture Name"/>
           <input type="submit" name="rename_this_video" class= "button" value="Rename Video"/>
 
