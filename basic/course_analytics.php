e5b3f4a2e92eeb10cf77dd6d05be5f7a8c0c0299
bikashsahoo1636 <bikashsahoo779@gmail.com>
2020-09-10 20:09:54 +0530

    front-end of course_analytics.php

---
+++ basic/course_analytics.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from educhamp.themetrades.com/demo/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 22 Feb 2019 13:08:15 GMT -->
+<head>
+
+	<!-- META ============================================= -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	
+	<!-- DESCRIPTION -->
+	<meta name="description" content="EduChamp : Education HTML Template" />
+	
+	<!-- OG -->
+	<meta property="og:title" content="EduChamp : Education HTML Template" />
+	<meta property="og:description" content="EduChamp : Education HTML Template" />
+	<meta property="og:image" content="" />
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- FAVICONS ICON ============================================= -->
+	
+	
+	<!-- PAGE TITLE HERE ============================================= -->
+	<title>Course Overview </title>
+	
+	<!-- MOBILE SPECIFIC ============================================= -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.min.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+	
+	<!-- All PLUGINS CSS ============================================= -->
+	<link rel="stylesheet" type="text/css" href="assets/css/assets.css">
+	<link rel="stylesheet" type="text/css" href="assets/vendors/calendar/fullcalendar.css">
+	
+	<!-- TYPOGRAPHY ============================================= -->
+	<link rel="stylesheet" type="text/css" href="assets/css/typography.css">
+	
+	<!-- SHORTCODES ============================================= -->
+	<link rel="stylesheet" type="text/css" href="assets/css/shortcodes/shortcodes.css">
+	
+	<!-- STYLESHEETS ============================================= -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/dashboard.css">
+	<link class="skin" rel="stylesheet" type="text/css" href="assets/css/color/color-1.css">
+	
+</head>
+<body class="ttr-opened-sidebar ttr-pinned-sidebar">
+
+	<!--Main container start -->
+	<main class="ttr-wrapper">
+		<div class="container-fluid">
+			<div class="db-breadcrumb">
+				<h4 class="breadcrumb-title">Course Overview</h4>
+				<ul class="db-breadcrumb-list">
+					<li><a href="#"><i class="fa fa-home"></i>Course</a></li>
+					<li>Dashboard</li>
+				</ul>
+			</div>	
+			<!-- Card -->
+			<div class="row">
+				<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
+					<div class="widget-card widget-bg1">					 
+						<div class="wc-item">
+							<h4 class="wc-title">
+								Total Students enrolled in the course
+							</h4>
+							<span class="wc-des">
+								.
+							</span>
+							<span class="wc-stats">
+								<span class="counter">18</span>M 
+							</span>		
+							
+						</div>				      
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
+					<div class="widget-card widget-bg2">					 
+						<div class="wc-item">
+							<h4 class="wc-title">
+								Students who have completed the course
+							</h4>
+							<span class="wc-des">
+								.
+							</span>
+							<span class="wc-stats">
+								<span class="counter">70</span>% 
+							</span>		
+							
+						</div>				      
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
+					<div class="widget-card widget-bg3">					 
+						<div class="wc-item">
+							<h4 class="wc-title">
+								Students who have collaborated  
+							</h4>
+							<span class="wc-des">
+								.
+							</span>
+							<span class="wc-stats">
+								<span class="counter">40</span>% 
+							</span>		
+							
+						</div>				      
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
+					<div class="widget-card widget-bg4">					 
+						<div class="wc-item">
+							<h4 class="wc-title">
+								Students who have engaged 
+							</h4>
+							<span class="wc-des">
+								.
+							</span>
+							<span class="wc-stats">
+								<span class="counter">30</span>% 
+							</span>		
+							
+						</div>				      
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-lg-3 col-xl-3 col-sm-6 col-12">
+					<div class="widget-card widget-bg5">					 
+						<div class="wc-item">
+							<h4 class="wc-title">
+								Students who have written notes
+							</h4>
+							<span class="wc-des">
+								.
+							</span>
+							<span class="wc-stats">
+								<span class="counter">30</span>% 
+							</span>		
+							
+						</div>				      
+					</div>
+				</div>
+			</div>
+			<!-- Card END -->
+			<div class="row">
+				<!-- Your Profile Views Chart -->
+				<div class="col-lg-8 m-b30">
+					<div class="widget-box">
+						<div class="wc-title">
+							<h4>Course Views</h4>
+						</div>
+						<div class="widget-inner">
+							<canvas id="chart" width="100" height="45"></canvas>
+						</div>
+					</div>
+				</div>
+				
+
+<!-- External JavaScripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/vendors/bootstrap/js/popper.min.js"></script>
+<script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="assets/vendors/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script>
+<script src="assets/vendors/magnific-popup/magnific-popup.js"></script>
+<script src="assets/vendors/counter/waypoints-min.js"></script>
+<script src="assets/vendors/counter/counterup.min.js"></script>
+<script src="assets/vendors/imagesloaded/imagesloaded.js"></script>
+<script src="assets/vendors/masonry/masonry.js"></script>
+<script src="assets/vendors/masonry/filter.js"></script>
+<script src="assets/vendors/owl-carousel/owl.carousel.js"></script>
+<script src='assets/vendors/scroll/scrollbar.min.js'></script>
+<script src="assets/js/functions.js"></script>
+<script src="assets/vendors/chart/chart.min.js"></script>
+<script src="assets/js/admin.js"></script>
+<script src='assets/vendors/calendar/moment.min.js'></script>
+<script src='assets/vendors/calendar/fullcalendar.js'></script>
+<script src='assets/vendors/switcher/switcher.js'></script>
+
+</body>
+
+<!-- Mirrored from educhamp.themetrades.com/demo/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 22 Feb 2019 13:09:05 GMT -->
+</html>
