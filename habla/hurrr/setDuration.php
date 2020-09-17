7245cd33b84d4b2697646480b201a733e3c8ef72
shuvo151dey <shuvo151dey@gmail.com>
2020-09-17 16:18:33 +0530

    repo clean

--- habla/hurrr/setDuration.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'connect.php';
-if (isset($_REQUEST['duration'])) {
-    echo $query = "UPDATE total_videos SET videolength=" . $_REQUEST['duration'] . " WHERE database_name='" . $db . "'";
-    if (!mysqli_query($link_central, $query)) {
-        echo ("Error description: " . mysqli_error($link));
-    }
-}
